df2a18cd44944748c9d6828459e8743bb6afb152
ravindar86 <ravindar@DESKTOP-JUI435A.localdomain>
2022-05-30 06:32:20 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+	const div = document.createElement('div');
+  render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+ // const linkElement = screen.getByText("test");
+ // expect(linkElement).toBeInTheDocument();
 });
+
+
